20bf5a4982446f11f0c98adb153ff68e3dceed87
huynhthien07 <22521383@gm.uit.edu.vn>
2024-12-22 20:36:30 +0700

    Create ResetPassword Page

--- morri/src/routes/PublicRoutes.tsx
+++ morri/src/routes/PublicRoutes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import ForgotPassword from "../pages/ForgotPassword/Forgotpassword";
 import NotFound from "../pages/NotFound/NotFound";
+import ResetPassword from "../pages/ResetPassword/ResetPassword";
 import { default as Register, default as Registration } from "../pages/SignUp/SignUpPage";
 
 export const PublicRoutes = () => {
@@ -11,6 +12,7 @@ export const PublicRoutes = () => {
       <Route path="/register" element={<Register />} />
       <Route path="/registeration" element={<Registration/>}/>
       <Route path="/forgotpassword" element={<ForgotPassword/>}/>
+      <Route path="/resetpassword" element={<ResetPassword/>}/>
       <Route path="*" element={<NotFound />} />
     </>
   );
