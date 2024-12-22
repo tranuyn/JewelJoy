e3e07ab2f0c4ee56bc879ed83b7edb137f3cccad
Gấu <115764988+bichngoc55@users.noreply.github.com>
2024-12-22 18:23:07 +0700

    Update

--- morri/src/routes/PublicRoutes.tsx
+++ morri/src/routes/PublicRoutes.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import ForgotPassword from "../pages/ForgotPassword/Forgotpassword";
 import NotFound from "../pages/NotFound/NotFound";
 import {
   default as Register,
   default as Registration,
 } from "../pages/SignUp/SignUpPage";
 import Forgotpassword from "../pages/ForgotPassword/Forgotpassword";
+import Login from "../pages/LoginPage/LoginPage";
 
 export const PublicRoutes = () => {
   return (
     <>
-       <Route path="/login" element={<Login />} /> 
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
- 
-      <Route path="/registeration" element={<Registration/>}/>
-      <Route path="/forgotpassword" element={<ForgotPassword/>}/>
- 
+
+      <Route path="/registeration" element={<Registration />} />
+      <Route path="/forgotpassword" element={<Forgotpassword />} />
+
       <Route path="*" element={<NotFound />} />
     </>
   );
