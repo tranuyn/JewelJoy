f41a2b2720b5176479b9d8a6e9f5ca57bbcfa711
Bảo Lingg <155095211+hniloablingg@users.noreply.github.com>
2024-12-30 18:12:36 +0700

    add navbar

--- morri/src/routes/CustomerRoutes.tsx
+++ morri/src/routes/CustomerRoutes.tsx
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import { ROLES } from "../constants/roles";
 import { ProtectedRoute } from "./ProtectedRoute";
 import CustomerLayout from "../component/Layout/CustomerLayout";
+import { Navbar } from "../component/Navbar/Navbar";
 
 export const CustomerRoutes = () => {
   return (
@@ -13,8 +14,8 @@ export const CustomerRoutes = () => {
         </ProtectedRoute>
       }
     >
-      {/* <Route path="/profile" element={<Profile />} />
-      <Route path="/orders" element={<Orders />} /> */}
+      {/* <Route path="/profile" element={<Profile />} /> */}
+      <Route path="/navbar" element={<Navbar />} />
     </Route>
   );
 };
